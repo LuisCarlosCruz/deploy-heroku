72b8ea2921d6e9feeb724bd97e7b3b1694618ecb
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-03-26 21:35:38 -0300

    dockerfile e yml

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.status(200).send('Hello World!'));
+app.get('/', (req, res) => res.status(200).send(`Hello Heroku! Port: ${port}`));
 
 app.listen(port, () => {
   console.log(`Aplicação ouvindo na porta http://localhost:${port}`);
